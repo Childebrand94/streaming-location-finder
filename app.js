8c2fe5b459b0a89bbf03424af1aca1fd12277b43
Chris Hildebrand <hildebrandc94@gmail.com>
2023-07-10 18:14:30 -0400

    Working on DOM manipulation to populate the screen when a title is searched

---
+++ app.js
@@ -0,0 +1,35 @@
+const getElement = (string) => {
+  return document.querySelector(string);
+};
+
+const getInput = () => {
+  const searchBar = getElement(".input_search");
+  const searchBtn = getElement(".btn_search");
+  searchBtn.addEventListener("click", (e) => {
+    return searchBar.value;
+  });
+};
+
+const options = {
+  method: "GET",
+  url: "https://streaming-availability.p.rapidapi.com/v2/search/title",
+  params: {
+    title: getInput(),
+    country: "us",
+    show_type: "movie",
+    output_language: "en",
+  },
+  headers: {
+    "X-RapidAPI-Key": "Your key here",
+    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+  },
+};
+
+const getMovie = async () => {
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
