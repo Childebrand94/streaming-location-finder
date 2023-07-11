0ef7901f7f6809da5f001ffbc31f30ab7bf863d9
Chris Hildebrand <hildebrandc94@gmail.com>
2023-07-11 17:00:05 -0400

    AXIOS request working for getMovie function

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const options = {
+  method: "GET",
+  url: "https://streaming-availability.p.rapidapi.com/v2/search/title",
+  params: {
+    title: "batman",
+    country: "us",
+    show_type: "movie",
+    output_language: "en",
+  },
+  headers: {
+    "X-RapidAPI-Key": "3b60a8e8f7mshdf601e24dafb99ep1f434ejsn080c28ad1a37",
+    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+  },
+};
+
 const getElement = (string) => {
   return document.querySelector(string);
 };
@@ -6,30 +21,36 @@ const getInput = () => {
   const searchBar = getElement(".input_search");
   const searchBtn = getElement(".btn_search");
   searchBtn.addEventListener("click", () => {
-    return searchBar.value;
+    console.log("Hello");
+    getMovie(searchBar.value);
   });
 };
 
-const options = {
-  method: "GET",
-  url: "https://streaming-availability.p.rapidapi.com/v2/search/title",
-  params: {
-    title: getInput(),
-    country: "us",
-    show_type: "movie",
-    output_language: "en",
-  },
-  headers: {
-    "X-RapidAPI-Key": "Your key here",
-    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-  },
-};
+getInput();
 
-const getMovie = async () => {
+const getMovie = async (title) => {
+  const movieOptions = {
+    ...options,
+    params: {
+      ...options.params,
+      title: title,
+    },
+  };
+  console.log(movieOptions);
   try {
-    const response = await axios.request(options);
-    return response.data;
+    const response = await axios.request(movieOptions);
+    console.log(response.data);
   } catch (error) {
     console.error(error);
   }
 };
+
+// const sendRequest = () => {
+//   const title = getInput(searchBtn, searchBar);
+//   options.params.title = title;
+//   searchBtn.addEventListener("click", () => {
+//     return getMovie();
+//   });
+// };
+
+// console.log(sendRequest());
