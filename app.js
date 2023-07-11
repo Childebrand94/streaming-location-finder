a873cfcbe38de88611d29276efd748f22332568f
Chris Hildebrand <hildebrandc94@gmail.com>
2023-07-11 17:21:48 -0400

    Replaced AXIOS with Fetch

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const options = {
   method: "GET",
-  url: "https://streaming-availability.p.rapidapi.com/v2/search/title",
-  params: {
-    title: "batman",
-    country: "us",
-    show_type: "movie",
-    output_language: "en",
-  },
   headers: {
     "X-RapidAPI-Key": "3b60a8e8f7mshdf601e24dafb99ep1f434ejsn080c28ad1a37",
     "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
   },
 };
-
+const getMovie = async (title) => {
+  const url = `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${title}&country=us&show_type=all&output_language=en`;
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
 const getElement = (string) => {
   return document.querySelector(string);
 };
@@ -28,29 +30,19 @@ const getInput = () => {
 
 getInput();
 
-const getMovie = async (title) => {
-  const movieOptions = {
-    ...options,
-    params: {
-      ...options.params,
-      title: title,
-    },
-  };
-  console.log(movieOptions);
-  try {
-    const response = await axios.request(movieOptions);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// const sendRequest = () => {
-//   const title = getInput(searchBtn, searchBar);
-//   options.params.title = title;
-//   searchBtn.addEventListener("click", () => {
-//     return getMovie();
-//   });
+// const getMovie = async (title) => {
+//   const movieOptions = {
+//     ...options,
+//     params: {
+//       ...options.params,
+//       title: title,
+//     },
+//   };
+//   console.log(movieOptions);
+//   try {
+//     const response = await axios.request(movieOptions);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
 // };
-
-// console.log(sendRequest());
